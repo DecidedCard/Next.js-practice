6d6b428f78db56baf2467bcd3c3801fad5678847
DecidedCard <heajun35@gmail.com>
2024-03-11 14:58:52 +0900

    [style] navigationbar css 작성

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,17 +3,17 @@ import React from "react";
 
 const NavigationBar = () => {
   return (
-    <nav>
-      <li>
+    <nav className="flex justify-center gap-2 text-xl list-none m-2">
+      <li className="rounded-xl hover:bg-slate-400 duration-300 w-14 text-center">
         <Link href={"/SSG"}>SSG</Link>
       </li>
-      <li>
+      <li className="rounded-xl hover:bg-slate-400 duration-300 w-14 text-center">
         <Link href={"/ISR"}>ISR</Link>
       </li>
-      <li>
+      <li className="rounded-xl hover:bg-slate-400 duration-300 w-14 text-center">
         <Link href={"/SSR"}>SSR</Link>
       </li>
-      <li>
+      <li className="rounded-xl hover:bg-slate-400 duration-300 w-14 text-center">
         <Link href={"/CSR"}>CSR</Link>
       </li>
     </nav>
