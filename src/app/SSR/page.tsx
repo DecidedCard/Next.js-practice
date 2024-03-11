d8cb81662e3fee6e21b5a1f7502ca4fff0d9e7dc
DecidedCard <heajun35@gmail.com>
2024-03-11 14:44:46 +0900

    [feat] SSR 작성

--- src/app/SSR/page.tsx
+++ src/app/SSR/page.tsx
@@ -1,7 +1,12 @@
+import { CatFactType } from "@/types";
 import React from "react";
 
-const SSRPage = () => {
-  return <div>SSRPage</div>;
+const SSRPage = async () => {
+  const response = await fetch(`https://catfact.ninja/fact`, {
+    cache: "no-cache",
+  });
+  const result: CatFactType = await response.json();
+  return <div>{result.fact}</div>;
 };
 
 export default SSRPage;
