c065b9f175b9603889ef6537cb3de28843013abd
DecidedCard <heajun35@gmail.com>
2024-03-11 14:42:32 +0900

    [feat] ISR 작성

--- src/app/ISR/page.tsx
+++ src/app/ISR/page.tsx
@@ -1,7 +1,14 @@
+import { CatFactType } from "@/types";
 import React from "react";
 
-const ISRPage = () => {
-  return <div>ISRPage</div>;
+const ISRPage = async () => {
+  const response = await fetch(`https://catfact.ninja/fact`, {
+    next: {
+      revalidate: 5,
+    },
+  });
+  const result: CatFactType = await response.json();
+  return <div>{result.fact}</div>;
 };
 
 export default ISRPage;
