89853ee6cda8870281ab55e9ca0ebb6d05a1a9ec
DecidedCard <heajun35@gmail.com>
2024-03-11 15:22:06 +0900

    [feat] header 작성

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="text-4xl font-bold text-center m-4">
+      Next.js Practice
+    </header>
+  );
+};
+
+export default Header;
