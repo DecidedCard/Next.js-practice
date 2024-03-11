e03a68d6f85951fc9e3cc263782f835404ed398a
DecidedCard <heajun35@gmail.com>
2024-03-11 14:49:47 +0900

    [feat] CSR 작성

--- src/app/CSR/page.tsx
+++ src/app/CSR/page.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client";
+
+import { CatFactType } from "@/types";
+import React, { useEffect, useState } from "react";
 
 const CSRPage = () => {
-  return <div>CSRPage</div>;
+  const [catFact, setCatFact] = useState<CatFactType | null>(null);
+  const fetchData = async () => {
+    const response = await fetch(`https://catfact.ninja/fact`);
+    const result: CatFactType = await response.json();
+    setCatFact(result);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  if (!catFact) {
+    return <div>로딩중입니다...</div>;
+  }
+  return <div>{catFact.fact}</div>;
 };
 
 export default CSRPage;
